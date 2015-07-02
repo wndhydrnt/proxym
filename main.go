0d1727892b0e2a257c17d6b10462d0d8cf701a59
wndhydrnt <hydrantanderwand@gmail.com>
2015-07-02 18:28:03 +0200

    Make HAProxy module work with other load balancers
    As the module is not only working with HAProxy, it has been renamed to Proxy.

--- main.go
+++ main.go
@@ -3,11 +3,11 @@ package main
 import (
 	_ "github.com/wndhydrnt/proxym/annotation_api"
 	_ "github.com/wndhydrnt/proxym/file"
-	_ "github.com/wndhydrnt/proxym/haproxy"
 	proxymLog "github.com/wndhydrnt/proxym/log"
 	"github.com/wndhydrnt/proxym/manager"
 	_ "github.com/wndhydrnt/proxym/marathon"
 	_ "github.com/wndhydrnt/proxym/mesos_master"
+	_ "github.com/wndhydrnt/proxym/proxy"
 	_ "github.com/wndhydrnt/proxym/signal"
 	"os"
 	"os/signal"
